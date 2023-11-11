01dbcc1981afc700cf87a20231dcd41924ac0df4
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-12 00:46:25 +0530

    InvalidHandshake error added

--- p2p/handshaker.go
+++ p2p/handshaker.go
@@ -1,6 +1,11 @@
 package p2p
 
-// HandshakeFunc.. ?
-type HandshakeFunc func(any) error
+import "errors"
 
-func NOPHandshakeFunc(any) error { return nil }
+// ErrInvalidHandhskae is returned if the handshake between the local and remote node could not be established
+var ErrInvalidHandshake = errors.New("invalid handshake")
+
+// HandshakeFunc.. ?
+type HandshakeFunc func(Peer) error
+
+func NOPHandshakeFunc(Peer) error { return nil }
