b65cae58692f9cf32271e57610e51db308628c4e
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-25 04:22:32 +0530

    delete file tested

--- store_test.go
+++ store_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"testing"
 )
@@ -12,7 +11,6 @@ func TestPathTransformFunc(t *testing.T) {
 	pathKey := CASPathTransformFunc(key)
 	expectedOriginalKey := "cf5d4b01c4d9438c22c56c832f83bd3e8c6304f9"
 	expectedPathName := "cf5d4/b01c4/d9438/c22c5/6c832/f83bd/3e8c6/304f9"
-	fmt.Println(pathKey)
 	if pathKey.Pathname != expectedPathName {
 		t.Errorf("have %s want %s", pathKey.Pathname, expectedPathName)
 	}
@@ -41,5 +39,21 @@ func TestStore(t *testing.T) {
 	if string(b) != string(data) {
 		t.Errorf("want %s have %s", data, b)
 	}
-	fmt.Println(string(b))
+}
+
+func TestStoreDeleteKey(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "wesleyspecial"
+
+	data := []byte("some jpeg")
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Errorf("Error while deleting")
+	}
 }
